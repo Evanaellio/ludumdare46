8482bdbf0ce7e15610f35d02b08049814c379215
2lafru <vincent@giraud.eu>
2020-04-19 00:47:54 +0200

    Gestion vie de l'ordi mère et écran bleu

--- PNJ/ordiMère.gd
+++ PNJ/ordiMère.gd
@@ -4,12 +4,20 @@ class_name ordiMere
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-var vie = 1000
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	get_node("2cran").play("oui",false)
 	pass # Replace with function body. 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	if get_node("HealthBar").health > 67:
+		get_node("2cran").play("oui",false)
+	elif get_node("HealthBar").health <= 67 and get_node("HealthBar").health > 33:
+		get_node("2cran").play("bof",false)
+	elif get_node("HealthBar").health <= 33 and get_node("HealthBar").health > 0:
+		get_node("2cran").play("non",false)
+	else:
+		get_node("2cran").play("mort",false)
+	pass
