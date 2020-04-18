04ef9ee28e5c94a3ab1ed444fec2beafe0dcfe2f
Tom Spreters <tom.spreters@gmail.com>
2020-04-18 20:01:14 +0200

    Les Câbles !

--- Prefabs/player/Player.gd
+++ Prefabs/player/Player.gd
@@ -7,6 +7,9 @@ var dir = Vector2.ZERO
 var moving = false
 var dir_scale = 1
 
+var current_cable: Cable = null
+var cable_prefab = load("res://Prefabs/Cable/Cable.tscn")
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -28,6 +31,15 @@ func _input(event):
 	else:
 		moving = false
 		animation.play("idle")
+	
+	if Input.is_action_just_pressed("Use"):
+		if current_cable == null:
+			current_cable = cable_prefab.instance()
+			current_cable.start_cable(position)
+			get_parent().add_child(current_cable)
+		else:
+			current_cable.end_cable()
+			current_cable = null
 
 func _physics_process(delta):
 	move_and_collide(dir * 2)
