f324c3e051027441226470308b32a15acb03738d
2lafru <vincent@giraud.eu>
2020-04-20 02:30:28 +0200

    écran de défaite fonctionnel quand le joueur meurt

--- GUI/WinLoseScreen/ecranPerdant.gd
+++ GUI/WinLoseScreen/ecranPerdant.gd
@@ -4,7 +4,7 @@ extends CanvasLayer
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-
+var activation = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -13,5 +13,12 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	if activation == 1:
+		$Control/Sprite.modulate.a = clamp($Control/Sprite.modulate.a + 0.005, 0, 1)
+	pass
+
+
+func _on_Player_death():
+	activation = 1
+	pass # Replace with function body.
